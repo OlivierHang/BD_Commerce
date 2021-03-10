6a38ae54806e91bab1b03becc93cbea93e80a60b
Olivier Hang <hang.sengolivier@gmail.com>
2021-03-10 16:10:33 +0100

    Inscriptions des users OK. TODO => partie Connexion

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,19 +4,55 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+    // C'est grace au "manager" de Doctrine qu'on peut faire nos manipulation avec la bd
+    private $entityManager;
+
+    // l'Entity manager est défini quand le Controller est appelé
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/register", name="register")
      */
-    public function index(): Response
+    // injection de dépendance => HttpFoundation\Request
+    public function index(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        // Step 1
+        // Permet d'écouter la requête entrante (objet Request de Symfony) pour voir s'il n'y a pas un Post
+        $form->handleRequest($request);
+
+        // Step 2
+        // Si le form est soumis et s'il est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Step 3
+            // Recupère les data du form et mettre ça dans la variable $user
+            $user = $form->getData();
+
+            // // Doctrine (ORM Symfony) permet l'interaction avec la bd
+            // $doctrine = $this->getDoctrine()->getManager();
+
+            // Step 4
+            // // Fige la data pour pouvoir l'enregistrer
+            // $doctrine->persist($user);
+            $this->entityManager->persist($user);
+            // // Execute la persistance / Enregistre la data dans la bd
+            // $doctrine->flush();
+            $this->entityManager->flush();
+        }
 
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,8 +16,17 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
+            ->add('email', EmailType::class, [
+                'label' => "Votre e-mail",
+                'required' => true
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => "le mot de passe et la confirmation doivent être identique",
+                'required' => true,
+                'first_options' => ['label' => "Votre mot de passe"],
+                'second_options' => ['label' => "Confirmez votre mot de passe"],
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire"
             ]);
