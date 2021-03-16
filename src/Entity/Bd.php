408a0f3d75dfc2c0b40b0c2b190736ba6b536fba
Olivier Hang <hang.sengolivier@gmail.com>
2021-03-16 14:40:09 +0100

    Creation entity 'Bd' + migration faite dans la bdd

---
+++ src/Entity/Bd.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BdRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BdRepository::class)
+ */
+class Bd
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ref;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $heros;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $prixPublic;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $prixEditeur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $genre;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $refEdit;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $editeur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $refFournisseur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fournisseur;
+
+    /**
+     * @ORM\Column(type="string", length=5000, nullable=true)
+     */
+    private $resume;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRef(): ?string
+    {
+        return $this->ref;
+    }
+
+    public function setRef(string $ref): self
+    {
+        $this->ref = $ref;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getHeros(): ?string
+    {
+        return $this->heros;
+    }
+
+    public function setHeros(?string $heros): self
+    {
+        $this->heros = $heros;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getPrixPublic(): ?float
+    {
+        return $this->prixPublic;
+    }
+
+    public function setPrixPublic(?float $prixPublic): self
+    {
+        $this->prixPublic = $prixPublic;
+
+        return $this;
+    }
+
+    public function getPrixEditeur(): ?float
+    {
+        return $this->prixEditeur;
+    }
+
+    public function setPrixEditeur(?float $prixEditeur): self
+    {
+        $this->prixEditeur = $prixEditeur;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(?string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getRefEdit(): ?string
+    {
+        return $this->refEdit;
+    }
+
+    public function setRefEdit(?string $refEdit): self
+    {
+        $this->refEdit = $refEdit;
+
+        return $this;
+    }
+
+    public function getEditeur(): ?string
+    {
+        return $this->editeur;
+    }
+
+    public function setEditeur(string $editeur): self
+    {
+        $this->editeur = $editeur;
+
+        return $this;
+    }
+
+    public function getRefFournisseur(): ?string
+    {
+        return $this->refFournisseur;
+    }
+
+    public function setRefFournisseur(?string $refFournisseur): self
+    {
+        $this->refFournisseur = $refFournisseur;
+
+        return $this;
+    }
+
+    public function getFournisseur(): ?string
+    {
+        return $this->fournisseur;
+    }
+
+    public function setFournisseur(string $fournisseur): self
+    {
+        $this->fournisseur = $fournisseur;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(?string $resume): self
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+}
